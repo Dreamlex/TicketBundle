76fc5111d64a60f40b15b207fd423bc2e9456902
Slava Ivanischenko <s.ivanischenko@gmail.com>
2016-08-01 12:08:09 +0300

    init Doctrine Collector

--- Entity/Message.php
+++ Entity/Message.php
@@ -69,8 +69,6 @@ class Message
 
     /**
      * @Assert\Valid()
-     * @ORM\ManyToOne(targetEntity="SellMMO\Sonata\MediaBundle\Entity\Media",cascade={"persist"})
-     * @ORM\JoinColumn(name="media_id", referencedColumnName="id")
      */
     private $media;
 
