91b0fcb028e1b13a7c1ebbd3e35a55791ab4dd55
Slava Ivanischenko <s.ivanischenko@gmail.com>
2016-08-05 10:03:18 +0300

    Переместил Test Entities

--- Tests/Functional/app/Entity/User.php
+++ Tests/Functional/app/Entity/User.php
@@ -6,7 +6,7 @@
  * Time: 9:35
  */
 
-namespace Dreamlex\TicketBundle\Tests\Functional\app\DefaultTestCase\Entity;
+namespace Dreamlex\TicketBundle\Tests\Functional\app\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Dreamlex\TicketBundle\Entity\Traits\UserTrait;
