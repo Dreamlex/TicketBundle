a0abef5a0ff9c15af32b1e4705d3cfead9a3c9a2
alex <alexndlm@gmail.com>
2016-10-20 15:09:09 +0300

    refactoring

--- Tests/Functional/TicketCreateTest.php
+++ Tests/Functional/TicketCreateTest.php
@@ -8,49 +8,12 @@
 
 namespace Dreamlex\Bundle\TicketBundle\Tests\Functional;
 
-use Symfony\Bundle\FrameworkBundle\Console\Application;
-use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\DomCrawler\Field\ChoiceFormField;
 use Symfony\Component\DomCrawler\Field\FileFormField;
-use Symfony\Component\DomCrawler\Field\InputFormField;
-use Symfony\Component\HttpKernel\Client;
+
 
 class TicketCreateTest extends TicketWebTestCase
 {
-    /** @var  Client */
-    protected $client;
-    const TEST_USER = 'test-user';
-    const TEST_PASSWORD = 'test-user';
-    const TICKET_ID = '6';
-
-    /**
-     * {@inheritDoc}
-     */
-
-    public static function setUpBeforeClass()
-    {
-        # get the container
-        $client = static::createClient(['test_case' => 'DefaultTestCase']);
-        /** @var Container $container */
-        $container = $client->getContainer();
-        $application = new Application($client->getKernel());
-
-        # create a cli application
-        self::runDoctrineSchema($application);
-        self::runFixMedia($application);
-        self::executeFixtures($container);
-    }
-
-    public function setUp()
-    {
-        $this->client = static::createClient(array('test_case' => 'DefaultTestCase'), [
-            'PHP_AUTH_USER' => self::TEST_USER,
-            'PHP_AUTH_PW' => self::TEST_PASSWORD,
-        ]);
-
-        parent::setUp();
-    }
-
     /**
      *  создания тикета с текстом
      */
@@ -190,7 +153,7 @@ class TicketCreateTest extends TicketWebTestCase
     {
         $client = static::createClient(['test_case' => 'DefaultTestCase']);
         $client->request('GET', '/ticket/create?_locale=ru');
-        self::assertEquals(401,$client->getResponse()->getStatusCode());
+        self::assertEquals(401, $client->getResponse()->getStatusCode());
     }
 
 }
--- Tests/Functional/TicketWebTestCase.php
+++ Tests/Functional/TicketWebTestCase.php
@@ -1,64 +1,92 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: dreamlex
- * Date: 11.08.16
- * Time: 16:13
- */
-
 namespace Dreamlex\Bundle\TicketBundle\Tests\Functional;
-
 
 use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\SchemaTool;
+use Dreamlex\Bundle\CoreBundle\Tests\FixtureAwareTestCaseTrait;
+use Dreamlex\Bundle\CoreBundle\Tests\WebTestCase;
 use Dreamlex\Bundle\TicketBundle\DataFixtures\ORM\LoadTicket;
 use Dreamlex\Bundle\TicketBundle\DataFixtures\ORM\LoadTicketCategories;
 use Dreamlex\Bundle\TicketBundle\DataFixtures\ORM\LoadTicketUsers;
 use Symfony\Bridge\Doctrine\DataFixtures\ContainerAwareLoader;
-use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\HttpKernel\Client;
 
-class TicketWebTestCase extends FunctionalWebTestCase
+class TicketWebTestCase extends WebTestCase
 {
+    use FixtureAwareTestCaseTrait;
+
+    /** @var  Client */
+    protected $client;
+
+    /**  */
+    public static function setUpBeforeClass()
+    {
+        parent::deleteTmpDir('UnitManager');
+
+        static::bootKernel();
+        static::$em = static::$kernel->getContainer()->get('doctrine')->getManager();
+
+        static::prepareSchema();
+
+        static::addFixture(new LoadUserStatusFixture());
+        static::executeFixtures();
+
+        $manipulator = static::$kernel->getContainer()->get('fos_user.util.user_manipulator');
+        $manipulator->create('admin', 'admin', 'test@example.com', true, false);
+        $manipulator->addRole('admin', 'ROLE_SUPER_ADMIN');
+    }
+
+    /**  */
+    public static function tearDownAfterClass()
+    {
+        parent::deleteTmpDir('UnitManager');
+    }
+
+    /**  */
+    public function setUp()
+    {
+        $this->client = static::createClient(['test_case' => 'Ticket'], [
+            'PHP_AUTH_USER' => 'test-user',
+            'PHP_AUTH_PW' => 'test-user',
+        ]);
+
+        parent::setUp();
+    }
+
+    /**  */
+    public function tearDown()
+    {
+        self::$em->clear();
+
+        parent::tearDown();
+    }
 
     /**
      * @var SchemaTool
      */
     protected static $schemaTool;
 
-    public static function runDoctrineSchema($application)
-    {
-
-        $application->setAutoExit(false);
-
-        $input = new ArrayInput(array(
-            'command' => 'doctrine:schema:update', '--dump-sql' => true, '--force' => true, '--complete' => true
-        ));
-
-        $output = new BufferedOutput();
-        $application->run($input, $output);
-    }
-
-    public static function runFixMedia($application)
-    {
-        $application->setAutoExit(false);
-
-        $input = new ArrayInput(array(
-            'command' => 'sonata:media:fix-media-context', '-vvv' => true
-        ));
-
-        $output = new BufferedOutput();
-        $application->run($input, $output);
-
-    }
+//    public static function runFixMedia($application)
+//    {
+//        $application->setAutoExit(false);
+//
+//        $input = new ArrayInput(array(
+//            'command' => 'sonata:media:fix-media-context', '-vvv' => true
+//        ));
+//
+//        $output = new BufferedOutput();
+//        $application->run($input, $output);
+//
+//    }
 
     /**
      * @param Container $container
      */
-    public static function executeFixtures($container){
+    public static function executeFixtures($container)
+    {
         $loader = new ContainerAwareLoader($container);
         $loader->addFixture(new LoadTicketUsers());
         $loader->addFixture(new LoadTicketCategories());
